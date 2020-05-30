c334a95d95142532ea06881d73e523104de1d723
Mỹ Duyên <myduyenn218@gmail.com>
2020-05-30 19:58:20 +0700

    Add exception

--- src/chapter2/agent_AB/Agent.java
+++ src/chapter2/agent_AB/Agent.java
@@ -2,8 +2,14 @@ package chapter2.agent_AB;
 
 public class Agent {
 	protected AgentProgram program;
+	private int points;
 
 	public Agent() {
+		points = 0;
+	}
+
+	public int getPoints() {
+		return points;
 	}
 
 	public Agent(AgentProgram aProgram) {
@@ -12,8 +18,17 @@ public class Agent {
 
 	public Action execute(Percept p) {
 		if (program != null) {
+			Action ac = program.execute(p);
+			if (ac == Environment.SUCK_DIRT) {
+				points += 500;
+			} else {
+				
+				points -= 10;
+
+			}
 			return program.execute(p);
 		}
+		points -= 100;
 		return NoOpAction.NO_OP;
 	}
 }
---
+++ src/chapter2/agent_AB/WrongPlaceException.java
@@ -0,0 +1,10 @@
+package chapter2.agent_AB;
+
+public class WrongPlaceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
