966b68e82ca4fd35a3c55fc9225794c5c38b2569
Mỹ Duyên <myduyenn218@gmail.com>
2020-05-30 19:39:29 +0700

    Revert "Finish task 2"
    This reverts commit 9bc8e58ea213584c3cc99051a410c699f7e72dd7.

--- src/chapter2/agent_AB/AgentProgram.java
+++ src/chapter2/agent_AB/AgentProgram.java
@@ -1,62 +1,25 @@
 package chapter2.agent_AB;
 
-import java.util.Random;
-
 public class AgentProgram {
 
 	public Action execute(Percept p) {// location, status
 		// TODO
 
 		if (p.getLocationState().equals(Environment.LocationState.DIRTY)) {
+
 			return Environment.SUCK_DIRT;
-		} else if (p.getLocationState().equals(Environment.LocationState.CLEAN)) {
+		} else if (p.getAgentLocation().equals("A")) {
 
-			Environment.Direction d = randomDirection();
-			
-			
-			switch (d) {
-			case LEFT:
-				return Environment.MOVE_LEFT;
+			return Environment.MOVE_RIGHT;
+		}
 
-			case RIGHT:
-				return Environment.MOVE_RIGHT;
+		else if (p.getAgentLocation().equals("B")) {
 
-			case UP:
-				return Environment.MOVE_UP;
-
-			case DOWN:
-				return Environment.MOVE_DOWN;
-			}
-			
-			
+			return Environment.MOVE_LEFT;
 
 		}
-		
 
 		return NoOpAction.NO_OP;
-	}
 
-	private Environment.Direction randomDirection() {
-
-		Random rd = new Random();
-		int number = rd.nextInt(4);
-		Environment.Direction direction = null;
-
-		switch (number) {
-		case 0:
-			direction = Environment.Direction.UP;
-			break;
-		case 1:
-			direction = Environment.Direction.DOWN;
-			break;
-		case 2:
-			direction = Environment.Direction.LEFT;
-			break;
-		case 3:
-			direction = Environment.Direction.RIGHT;
-			break;
-		}
-
-		return direction;
 	}
 }
--- src/chapter2/agent_AB/Environment.java
+++ src/chapter2/agent_AB/Environment.java
@@ -1,42 +1,30 @@
 package chapter2.agent_AB;
 
+
 public class Environment {
 	public static final Action MOVE_LEFT = new DynamicAction("LEFT");
 	public static final Action MOVE_RIGHT = new DynamicAction("RIGHT");
 	public static final Action MOVE_UP = new DynamicAction("UP");
 	public static final Action MOVE_DOWN = new DynamicAction("DOWN");
 	public static final Action SUCK_DIRT = new DynamicAction("SUCK");
+	public static final String LOCATION_A = "A";
+	public static final String LOCATION_B = "B";
 
 	public enum LocationState {
 		CLEAN, DIRTY, OBSTACLE
 	}
 
-	public enum Direction {
-		UP, DOWN, LEFT, RIGHT
-	}
-
 	private EnvironmentState envState;
-	private int m;
-	private int n;
 	private boolean isDone = false;// all squares are CLEAN
 	private Agent agent = null;
 
-	public Environment(Environment.LocationState[][] grid, int totalDirt) {
-		m = grid.length;
-		n = grid[0].length;
-
-		envState = new EnvironmentState(grid, totalDirt);
+	public Environment(LocationState locAState, LocationState locBState) {
+		envState = new EnvironmentState(locAState, locBState);
 	}
 
 	// add an agent into the enviroment
-	public void addAgent(Agent agent, String location) throws WrongPlaceException {
+	public void addAgent(Agent agent, String location) {
 		// TODO
-		System.out.println(location);
-
-		if (envState.getLocationState(location) == LocationState.OBSTACLE) {
-			throw new WrongPlaceException();
-		}
-
 		this.agent = agent;
 		envState.setAgentLocation(location);
 	}
@@ -48,60 +36,18 @@ public class Environment {
 	// Update enviroment state when agent do an action
 	public EnvironmentState executeAction(Action action) {
 		// TODO
-		String agentLocation = envState.getAgentLocation();
-		if (action.equals(SUCK_DIRT)) {
-
-			envState.setLocationState(agentLocation, LocationState.CLEAN);
-			envState.setCurrentDirt(envState.getCurrentDirt() - 1);
-
-		} else {
-			String nextLocation = null;
-
-			if (action.equals(MOVE_LEFT)) {
-				nextLocation = getNextLocation(Direction.LEFT, agentLocation);
-			} else if (action.equals(MOVE_RIGHT)) {
-				nextLocation = getNextLocation(Direction.RIGHT, agentLocation);
-			} else if (action.equals(MOVE_UP)) {
-				nextLocation = getNextLocation(Direction.UP, agentLocation);
-			} else if (action.equals(MOVE_DOWN)) {
-				nextLocation = getNextLocation(Direction.DOWN, agentLocation);
-			}
-
-			if (nextLocation != null && envState.getLocationState(nextLocation) != LocationState.OBSTACLE) {
-
-				envState.setAgentLocation(nextLocation);
-			}
+		if(action.equals(MOVE_LEFT)) {
+			envState.setAgentLocation(LOCATION_A);
+			envState.setLocationState(LOCATION_A, envState.getLocationState(LOCATION_A));
+		} else if(action.equals(MOVE_RIGHT)) {
+			envState.setAgentLocation(LOCATION_B);
+			envState.setLocationState(LOCATION_B, envState.getLocationState(LOCATION_B));
+		}else if(action.equals(SUCK_DIRT)) {
+			envState.setLocationState(envState.getAgentLocation(), LocationState.CLEAN);
+		} else if (action.isNoOp()) {
+			
 		}
-
 		return envState;
-	}
-
-	private String getNextLocation(Environment.Direction direction, String location) {
-		String[] loc = location.split(",");
-		int i = Integer.parseInt(loc[0]);
-		int j = Integer.parseInt(loc[1]);
-
-		switch (direction) {
-		case UP:
-			i--;
-			break;
-		case DOWN:
-			i++;
-			break;
-		case LEFT:
-			j--;
-			break;
-		case RIGHT:
-			j++;
-			break;
-		default:
-			break;
-		}
-		if (i < 0 || j < 0 || i >= m || j >= n) {
-			return null;
-		} else
-			return EnvironmentState.getKeyState(i, j);
-
 	}
 
 	// get percept<AgentLocation, LocationState> at the current location where agent
@@ -113,29 +59,23 @@ public class Environment {
 	}
 
 	public void step() {
-		envState.display(m, n);
+		envState.display();
 		String agentLocation = this.envState.getAgentLocation();
 		Action anAction = agent.execute(getPerceptSeenBy());
 		EnvironmentState es = executeAction(anAction);
 
 		System.out.println("Agent Loc.: " + agentLocation + "\tAction: " + anAction);
 
-//		int durt 
-
-		if (envState.getCurrentDirt() == 0)
+		if ((es.getLocationState(LOCATION_A) == LocationState.CLEAN)
+				&& (es.getLocationState(LOCATION_B) == LocationState.CLEAN))
 			isDone = true;// if both squares are clean, then agent do not need to do any action
-		es.display(m, n);
+		es.display();
 	}
 
 	public void step(int n) {
 		for (int i = 0; i < n; i++) {
 			step();
-
 			System.out.println("-------------------------");
-			if (isDone) {
-				System.out.println("Done");
-				break;
-			}
 		}
 	}
 
@@ -145,7 +85,6 @@ public class Environment {
 		while (!isDone) {
 			System.out.println("step: " + i++);
 			step();
-			System.out.println("-------------------------");
 		}
 	}
 }
